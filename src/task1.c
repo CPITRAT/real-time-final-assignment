f723d23b31a2a0da59a9ed79d4b9b0ac9ace1132
Clément Pitrat <clement.pitrat@ipsa.fr>
2026-04-27 23:48:29 +0200

    Clean results and finalize project structure

--- src/task1.c
+++ src/task1.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define N 200000  // Ajuste si tu veux ralentir/accélérer
+#define N 200000  // Adjust this value to make execution slower/faster
 
 int main() {
     volatile double x = 1.001;
@@ -12,13 +12,13 @@ int main() {
 
     for (long long i = 0; i < N; i++) {
         result *= x;
-        if (result > 1e6) result = 1.0; // éviter overflow
+        if (result > 1e6) result = 1.0; // prevent overflow
     }
 
     clock_t end = clock();
 
     double time_us = (double)(end - start) * 1e6 / CLOCKS_PER_SEC;
 
-    printf("%.3f\n", time_us); // temps en microsecondes
+    printf("%.3f\n", time_us); // execution time in microseconds
     return 0;
 }
